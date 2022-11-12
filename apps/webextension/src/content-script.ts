c062f5b2f9c0406329f86492df591037a539b304
duncannah <38371914+duncannah@users.noreply.github.com>
2022-11-12 16:57:10 +0100

    fix: use shadow DOM to insert popup

--- apps/webextension/src/content-script.ts
+++ apps/webextension/src/content-script.ts
@@ -4,16 +4,19 @@ import * as ReactDOM from "react-dom/client";
 
 import App from "./interface/app";
 
-(() => {
+(async () => {
 	if (`__OCRTRANSLATE__` in window) return;
 
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
 	window[`__OCRTRANSLATE__`] = true;
 
-	const rootEl = document.createElement(`div`);
-	rootEl.id = `__OCRTRANSLATE__`;
-	document.body.append(rootEl);
+	const el = document.createElement(`div`);
+	const shadowRoot = el.attachShadow({ mode: `open` });
+
+	shadowRoot.innerHTML = `<link rel="stylesheet" type="text/css" href="${chrome.runtime.getURL(`content-script.css`)}"></link><div id='root'></div>`;
+
+	document.body.append(el);
 
 	chrome.runtime.sendMessage({ greeting: `hello` }, function (response) {
 		const img = new Image();
@@ -22,7 +25,11 @@ import App from "./interface/app";
 		document.body.append(img);
 	});
 
-	const root = ReactDOM.createRoot(rootEl);
+	const rootEl = shadowRoot.getElementById(`root`);
 
-	root.render(React.createElement(StrictMode, {}, React.createElement(App)));
+	if (rootEl) {
+		const root = ReactDOM.createRoot(rootEl);
+
+		root.render(React.createElement(StrictMode, {}, React.createElement(App)));
+	}
 })();
